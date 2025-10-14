9bd68e1932a4193755f4c1e1952367fc064fe1d2
officialcbk <eeze@academic.rrc.ca>
2025-10-14 12:17:36 -0500

    Payment strategy completed

--- patterns/strategy/payment_strategy.py
+++ patterns/strategy/payment_strategy.py
@@ -1,4 +1,24 @@
 """This module defines the PaymentStrategy class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Emmanuel Eze"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+from billing_account.billing_account import BillingAccount
+from payee.payee import Payee
+
+class PaymentStrategy(ABC):
+    """
+    Class for payment strategy
+    """
+    @abstractmethod
+    def process_payment(self, account:BillingAccount, payee:Payee, amount) -> str:
+        """
+        account: Billing account
+        payee: Payee
+        amount: Amount to be paid
+
+        return: str
+        """
+        pass
+
