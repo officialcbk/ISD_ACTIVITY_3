bea0923c66c3464f644c493d41eba79bc394ff85
officialcbk <eeze@academic.rrc.ca>
2025-10-15 08:09:14 -0500

    Partialpayment completed

--- patterns/strategy/partial_payment_strategy.py
+++ patterns/strategy/partial_payment_strategy.py
@@ -1,4 +1,11 @@
 """This module defines the PartialPaymentStrategy class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Emmanuel Eze"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+from billing_account.billing_account import BillingAccount  
+from payee.payee import Payee
+from patterns.strategy.payment_strategy import PaymentStrategy
+
+class PartialPaymentStrategy(PaymentStrategy):
