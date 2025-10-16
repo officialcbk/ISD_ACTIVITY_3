c2a3cc718286f42601d261c97b28c203088baff2
officialcbk <eeze@academic.rrc.ca>
2025-10-16 12:51:41 -0500

    Payments Strategy implemented

--- payment/payment.py
+++ payment/payment.py
@@ -1,7 +1,39 @@
 """This module defines the Payment class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Emmanuel Eze"
+__version__ = "1.0.0"
 
 from billing_account.billing_account import BillingAccount
 from payee.payee import Payee
+from patterns.strategy.payment_strategy import PaymentStrategy
+
+class Payment:
+    def __init__(self, strategy: PaymentStrategy):
+        """
+        Initializes a Payment strategy.
+
+        Args:
+            strategy (PaymentStrategy): The strategy to be used for processing payments.
+
+        Raises:
+            ValueError: If the strategy provided is not a valid PaymentStrategy instance.
+        """
+
+        if isinstance(strategy, PaymentStrategy):
+            self.strategy = strategy
+        else:
+            raise ValueError("Invalid Strategy")
+
+    def pay_bill(self, account:BillingAccount, payee:Payee, amount: float) -> str:
+        """
+        Implements the payment using the assigned strategy.
+
+        Args:
+            account (BillingAccount): The billing account to apply the payment to.
+            payee (Payee): The payee receiving the payment.
+            amount (float): The amount to be paid.
+
+        Returns:
+            str: The confirmation message from the applied strategy.
+        """
+        return self.strategy.process_payment(account, payee, amount)
